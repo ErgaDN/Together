5b679301d1bedf7f4feedbb96c09b5fda5c49e69
Renana Turgeman <renanaturgeman058@gmail.com>
2024-01-22 19:44:33 +0200

    add editing products screen

--- app/src/main/java/com/example/together/EditingProducts.java
+++ app/src/main/java/com/example/together/EditingProducts.java
@@ -3,12 +3,17 @@ package com.example.together;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class EditingProducts extends AppCompatActivity {
-
+    Button btn_adding_products;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editing_products);
+        btn_adding_products = findViewById(R.id.btn_adding_products);
+
+        //TODO: add onclick to btn_adding_products
+
     }
 }
