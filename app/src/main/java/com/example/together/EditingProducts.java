4935b4362ffa58a221ce227a1635fbc14b1cd784
Renana Turgeman <renanaturgeman058@gmail.com>
2024-01-22 19:12:15 +0200

    Creating a screen for the three agricultural processions

---
+++ app/src/main/java/com/example/together/EditingProducts.java
@@ -0,0 +1,14 @@
+package com.example.together;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EditingProducts extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_editing_products);
+    }
+}
