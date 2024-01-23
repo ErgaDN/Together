8964c55810405c8cc8629a452a99df386fb7042e
Renana Turgeman <renanaturgeman058@gmail.com>
2024-01-23 16:05:31 +0200

    add logout button for the argricultural

--- app/src/main/java/com/example/together/OrderSummary.java
+++ app/src/main/java/com/example/together/OrderSummary.java
@@ -13,10 +13,25 @@ import android.widget.Button;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class OrderSummary extends Fragment {
+    Button button;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_order_summary, container, false);
+        View view = inflater.inflate(R.layout.fragment_order_summary, container, false);
+
+        button = view.findViewById(R.id.logout);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                Intent intent = new Intent(requireContext(), Login.class);
+                startActivity(intent);
+                getActivity().finish();
+            }
+        });
+
+
+        return view;
     }
 }
