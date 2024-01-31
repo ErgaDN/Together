68e58c9cb5cd85ad34c0a7b11f63c20f3b45b2ce
Renana Turgeman <renanaturgeman058@gmail.com>
2024-01-31 15:27:22 +0200

    Category update in seller

--- app/src/main/java/com/example/together/UpdateProduct.java
+++ app/src/main/java/com/example/together/UpdateProduct.java
@@ -3,8 +3,10 @@ package com.example.together;
 import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -34,7 +36,7 @@ import java.util.Objects;
 public class UpdateProduct extends AppCompatActivity {
     String value;
     EditText editTitle, editDescription, editQuantity, editPrice;
-    String title, description, quantity, price;
+    String title, category, description, quantity, price;
     TextView editCategory;
     Button btn_updateproduct;
     private FirebaseAuth firebaseAuth;
@@ -60,19 +62,29 @@ public class UpdateProduct extends AppCompatActivity {
         getSupportActionBar().setTitle("עדכון מוצר");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        //get the IDproduct from the other activity
         Bundle extras = getIntent().getExtras();
         if(extras != null){
             value = extras.getString("IDproduct");
         }
 
+        editCategory.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //pick category
+                categoryDialog();
+            }
+        });
+
         btn_updateproduct.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                boolean isTitleChanged;
+                boolean isTitleChanged, isCategoryChanged;
                 isTitleChanged = isTitleChanged();
+                isCategoryChanged = isCategoryChanged();
 
 
-                if (isTitleChanged ) {
+                if (isTitleChanged || isCategoryChanged ) {
                     Toast.makeText(UpdateProduct.this, "Saved", Toast.LENGTH_SHORT).show();
                 } else {
                     Toast.makeText(UpdateProduct.this, "No Changes Found", Toast.LENGTH_SHORT).show();
@@ -97,42 +109,105 @@ public class UpdateProduct extends AppCompatActivity {
         }
     }
 
-private void updateTitle(String title) {
-    String userID = Objects.requireNonNull(firebaseAuth.getCurrentUser()).getUid();
-
-    // Reference to the "products" collection
-    CollectionReference productsRef = db.collection("seller")
-            .document(userID)
-            .collection("products");
-
-    // Query to find the document where "productId" matches the given value
-    Query query = productsRef.whereEqualTo("productId", value);
-
-    query.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-        @Override
-        public void onComplete(@NonNull Task<QuerySnapshot> task) {
-            if (task.isSuccessful()) {
-                for (QueryDocumentSnapshot document : task.getResult()) {
-                    // Update the "productTitle" field in the found document
-                    document.getReference().update("productTitle", title)
-                            .addOnSuccessListener(new OnSuccessListener<Void>() {
-                                @Override
-                                public void onSuccess(Void unused) {
-                                    Log.d(TAG, "Title update successful for user: " + userID);
-                                }
-                            })
-                            .addOnFailureListener(new OnFailureListener() {
-                                @Override
-                                public void onFailure(@NonNull Exception e) {
-                                    Log.e(TAG, "Title update failed for user: " + userID, e);
-                                }
-                            });
-                }
-            } else {
-                Log.e(TAG, "Error getting documents: ", task.getException());
-            }
+    public boolean isCategoryChanged(){
+        if (editCategory != null && editCategory.length() > 0){//
+            category = editCategory.getText().toString();
+            updateCategory(category);
+            return true;
+        } else{
+            return false;
         }
-    });
-}
+    }
+
+    private void updateTitle(String title) {
+        String userID = Objects.requireNonNull(firebaseAuth.getCurrentUser()).getUid();
+
+        // Reference to the "products" collection
+        CollectionReference productsRef = db.collection("seller")
+                .document(userID)
+                .collection("products");
+
+        // Query to find the document where "productId" matches the given value
+        Query query = productsRef.whereEqualTo("productId", value);
+
+        query.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()) {
+                    for (QueryDocumentSnapshot document : task.getResult()) {
+                        // Update the "productTitle" field in the found document
+                        document.getReference().update("productTitle", title)
+                                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                    @Override
+                                    public void onSuccess(Void unused) {
+                                        Log.d(TAG, "Title update successful for user: " + userID);
+                                    }
+                                })
+                                .addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        Log.e(TAG, "Title update failed for user: " + userID, e);
+                                    }
+                                });
+                    }
+                } else {
+                    Log.e(TAG, "Error getting documents: ", task.getException());
+                }
+            }
+        });
+    }
+
+    private void updateCategory(String category) {
+        String userID = Objects.requireNonNull(firebaseAuth.getCurrentUser()).getUid();
+
+        // Reference to the "products" collection
+        CollectionReference productsRef = db.collection("seller")
+                .document(userID)
+                .collection("products");
+
+        // Query to find the document where "productId" matches the given value
+        Query query = productsRef.whereEqualTo("productId", value);
+
+        query.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()) {
+                    for (QueryDocumentSnapshot document : task.getResult()) {
+                        // Update the "productTitle" field in the found document
+                        document.getReference().update("productCategory", category)
+                                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                    @Override
+                                    public void onSuccess(Void unused) {
+                                        Log.d(TAG, "Category update successful for user: " + userID);
+                                    }
+                                })
+                                .addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        Log.e(TAG, "Category update failed for user: " + userID, e);
+                                    }
+                                });
+                    }
+                } else {
+                    Log.e(TAG, "Error getting documents: ", task.getException());
+                }
+            }
+        });
+    }
+
+
+    private void categoryDialog() {
+        //dialog
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Product Category").setItems(Constants.productCategories, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                //set picked category
+                String selectedCategory = Constants.productCategories[which];
+                //set picked category
+                editCategory.setText(selectedCategory);
+            }
+        }).show();
+    }
 
 }
