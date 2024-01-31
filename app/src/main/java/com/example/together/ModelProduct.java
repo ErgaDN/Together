b8ceec6d29efa50df6681424a85b49d1d3aad3e2
ErgaDN <erga932@gmail.com>
2024-01-31 15:40:09 +0200

    ModelProduct

---
+++ app/src/main/java/com/example/together/ModelProduct.java
@@ -0,0 +1,84 @@
+package com.example.together;
+
+public class ModelProduct {
+    private String productId, productTitle, productDescription,
+            productCategory, productQuantity, productPrice, timestamp, uid;
+
+    public ModelProduct(String productId, String productTitle, String productDescription, String productCategory, String productQuantity, String productPrice, String timestamp, String uid) {
+        this.productId = productId;
+        this.productTitle = productTitle;
+        this.productDescription = productDescription;
+        this.productCategory = productCategory;
+        this.productQuantity = productQuantity;
+        this.productPrice = productPrice;
+        this.timestamp = timestamp;
+        this.uid = uid;
+    }
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+
+    public String getProductTitle() {
+        return productTitle;
+    }
+
+    public void setProductTitle(String productTitle) {
+        this.productTitle = productTitle;
+    }
+
+    public String getProductDescription() {
+        return productDescription;
+    }
+
+    public void setProductDescription(String productDescription) {
+        this.productDescription = productDescription;
+    }
+
+    public String getProductCategory() {
+        return productCategory;
+    }
+
+    public void setProductCategory(String productCategory) {
+        this.productCategory = productCategory;
+    }
+
+    public String getProductQuantity() {
+        return productQuantity;
+    }
+
+    public void setProductQuantity(String productQuantity) {
+        this.productQuantity = productQuantity;
+    }
+
+    public String getProductPrice() {
+        return productPrice;
+    }
+
+    public void setProductPrice(String productPrice) {
+        this.productPrice = productPrice;
+    }
+
+    public String getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(String timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public ModelProduct() {
+    }
+}
