c400f8e1f293390315f26cd317c3a0404b0acaa7
Renana Turgeman <renanaturgeman058@gmail.com>
2024-02-01 10:51:08 +0200

    remove Main Activity as launcher

--- app/src/main/java/com/example/together/Login.java
+++ app/src/main/java/com/example/together/Login.java
@@ -32,21 +32,6 @@ public class Login extends AppCompatActivity {
 
     String userID;
 
-
-    @Override
-    public void onStart() {
-        super.onStart();
-        // Check if user is signed in (non-null) and update UI accordingly.
-        FirebaseUser currentUser = mAuth.getCurrentUser();
-        if (currentUser != null) {
-            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-            startActivity(intent);
-            finish();
-        }
-    }
-
-
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
