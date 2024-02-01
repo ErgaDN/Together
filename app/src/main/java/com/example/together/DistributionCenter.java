13cc78a49e4eb0a7c481bc2ebf1eeffc6fd3564e
Renana Turgeman <renanaturgeman058@gmail.com>
2024-02-01 10:25:52 +0200

    add toolbar to the client

--- app/src/main/java/com/example/together/DistributionCenter.java
+++ app/src/main/java/com/example/together/DistributionCenter.java
@@ -1,147 +1,147 @@
-package com.example.together;
-
-import static android.content.ContentValues.TAG;
-
-import androidx.appcompat.app.AlertDialog;
-import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.RecyclerView;
-
-import android.content.DialogInterface;
-import android.content.Intent;
-import android.os.Bundle;
-import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.EditText;
-import android.widget.ImageButton;
-import android.widget.TextView;
-
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.material.card.MaterialCardView;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-import com.google.firebase.firestore.CollectionReference;
-import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.FirebaseFirestore;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Objects;
-import java.util.zip.Inflater;
-
-import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
-
-public class DistributionCenter extends Fragment {
-
-  private EditText searchProductsEt;
-  private ImageButton filterProductBtn;
-  private TextView filteredProductsTv;
-  private RecyclerView productsRv;
-  private ArrayList<ModelProduct> productList;
-  private AdapterProductSeller adapterProductSeller;
-  private FirebaseAuth firebaseAuth;
-  ///TODO:-----50:00-----
-  public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                           Bundle savedInstanceState) {
-    // Inflate the layout for this fragment
-    View view = inflater.inflate(R.layout.fragment_distribution_center, container, false);
-    searchProductsEt = view.findViewById(R.id.searchProductsEt);
-    filterProductBtn = view.findViewById(R.id.filterProductBtn);
-    filteredProductsTv = view.findViewById(R.id.filteredProductsTv);
-    productsRv = view.findViewById(R.id.productsRv);
-
-    loadAllProducts();
-
-    filterProductBtn.setOnClickListener(new View.OnClickListener() {
-      @Override
-      public void onClick(View v) {
-        AlertDialog.Builder builder = new AlertDialog.Builder(DistributionCenter.this);
-        builder.setTitle("Choose Category:").setItems(Constants.productCategories, new DialogInterface.OnClickListener() {
-          @Override
-          public void onClick(DialogInterface dialog, int which) {
-            //get selected item
-            String selected = Constants.productCategories_1[which];
-            filteredProductsTv.setText(selected);
-            if (selected.equals("הכל")) {
-              /////TODO: 55:17------------
-              loadAllProducts();
-            }
-            else {
-              loadFilteredProducts(selected);
-            }
-          }
-        });
-      }
-    });
-
-    return view;
-
-  }
-
-  private void loadFilteredProducts(String selected) {
-    productList = new ArrayList<>();
-
-    //get all product
-    DatabaseReference reference = FirebaseDatabase.getInstance().getReference("User");
-    reference.child(firebaseAuth.getUid()).child("Products")
-            .addChildEventListener(new ValueEventListener() {
-              @Override
-              public void onDataChange(@Nonnull DataSnapshot dataSnapshot) {
-                //before getting rest list
-                productList.clear();
-                for (DataSnapshot ds: dataSnapshot.getChildren()) {
-                  String productCategory = "" + ds.child("productCategory").getValue();
-                  if (selected.equals(productCategory))
-
-                  ModelProduct modelProduct = ds.getValue(ModelProduct.class);
-                  productList.add(modelProduct);
-                }
-                //setup adapter
-                productsRv.setAdapter(adapterProductSeller);
-                //set adapter
-                productsRv.setAdapter(adapterProductSeller);
-              }
-
-              @Override
-              public void onCancelled(@Nonnull DatabaseError databaseError) {
-
-              }
-            });
-  }
-
-
-  private void loadAllProducts() {
-    productList = new ArrayList<>();
-
-    //get all product
-    DatabaseReference reference = FirebaseDatabase.getInstance().getReference("User");
-    reference.child(firebaseAuth.getUid()).child("Products")
-            .addChildEventListener(new ValueEventListener() {
-              @Override
-              public void onDataChange(@Nonnull DataSnapshot dataSnapshot) {
-                //before getting rest list
-                productList.clear();
-                for (DataSnapshot ds: dataSnapshot.getChildren()) {
-                  ModelProduct modelProduct = ds.getValue(ModelProduct.class);
-                  productList.add(modelProduct);
-                }
-                //setup adapter
-                productsRv.setAdapter(adapterProductSeller);
-                //set adapter
-                productsRv.setAdapter(adapterProductSeller);
-              }
-
-              @Override
-              public void onCancelled(@Nonnull DatabaseError databaseError) {
-
-              }
-            });
-  }
-
-}
+//package com.example.together;
+//
+//import static android.content.ContentValues.TAG;
+//
+//import androidx.appcompat.app.AlertDialog;
+//import androidx.fragment.app.Fragment;
+//import androidx.recyclerview.widget.RecyclerView;
+//
+//import android.content.DialogInterface;
+//import android.content.Intent;
+//import android.os.Bundle;
+//import android.util.Log;
+//import android.view.LayoutInflater;
+//import android.view.View;
+//import android.view.ViewGroup;
+//import android.widget.EditText;
+//import android.widget.ImageButton;
+//import android.widget.TextView;
+//
+//import com.google.android.gms.tasks.OnSuccessListener;
+//import com.google.android.material.card.MaterialCardView;
+//import com.google.firebase.auth.FirebaseAuth;
+//import com.google.firebase.database.DataSnapshot;
+//import com.google.firebase.database.DatabaseError;
+//import com.google.firebase.database.DatabaseReference;
+//import com.google.firebase.database.FirebaseDatabase;
+//import com.google.firebase.database.ValueEventListener;
+//import com.google.firebase.firestore.CollectionReference;
+//import com.google.firebase.firestore.DocumentReference;
+//import com.google.firebase.firestore.FirebaseFirestore;
+//
+//import java.util.ArrayList;
+//import java.util.HashMap;
+//import java.util.Objects;
+//import java.util.zip.Inflater;
+//
+//import javax.annotation.Nonnull;
+//import javax.annotation.Nullable;
+//
+//public class DistributionCenter extends Fragment {
+//
+//  private EditText searchProductsEt;
+//  private ImageButton filterProductBtn;
+//  private TextView filteredProductsTv;
+//  private RecyclerView productsRv;
+//  private ArrayList<ModelProduct> productList;
+//  private AdapterProductSeller adapterProductSeller;
+//  private FirebaseAuth firebaseAuth;
+//  ///TODO:-----50:00-----
+//  public View onCreateView(LayoutInflater inflater, ViewGroup container,
+//                           Bundle savedInstanceState) {
+//    // Inflate the layout for this fragment
+//    View view = inflater.inflate(R.layout.fragment_distribution_center, container, false);
+//    searchProductsEt = view.findViewById(R.id.searchProductsEt);
+//    filterProductBtn = view.findViewById(R.id.filterProductBtn);
+//    filteredProductsTv = view.findViewById(R.id.filteredProductsTv);
+//    productsRv = view.findViewById(R.id.productsRv);
+//
+//    loadAllProducts();
+//
+//    filterProductBtn.setOnClickListener(new View.OnClickListener() {
+//      @Override
+//      public void onClick(View v) {
+//        AlertDialog.Builder builder = new AlertDialog.Builder(DistributionCenter.this);
+//        builder.setTitle("Choose Category:").setItems(Constants.productCategories, new DialogInterface.OnClickListener() {
+//          @Override
+//          public void onClick(DialogInterface dialog, int which) {
+//            //get selected item
+//            String selected = Constants.productCategories_1[which];
+//            filteredProductsTv.setText(selected);
+//            if (selected.equals("הכל")) {
+//              /////TODO: 55:17------------
+//              loadAllProducts();
+//            }
+//            else {
+//              loadFilteredProducts(selected);
+//            }
+//          }
+//        });
+//      }
+//    });
+//
+//    return view;
+//
+//  }
+//
+//  private void loadFilteredProducts(String selected) {
+//    productList = new ArrayList<>();
+//
+//    //get all product
+//    DatabaseReference reference = FirebaseDatabase.getInstance().getReference("User");
+//    reference.child(firebaseAuth.getUid()).child("Products")
+//            .addChildEventListener(new ValueEventListener() {
+//              @Override
+//              public void onDataChange(@Nonnull DataSnapshot dataSnapshot) {
+//                //before getting rest list
+//                productList.clear();
+//                for (DataSnapshot ds: dataSnapshot.getChildren()) {
+//                  String productCategory = "" + ds.child("productCategory").getValue();
+//                  if (selected.equals(productCategory))
+//
+//                  ModelProduct modelProduct = ds.getValue(ModelProduct.class);
+//                  productList.add(modelProduct);
+//                }
+//                //setup adapter
+//                productsRv.setAdapter(adapterProductSeller);
+//                //set adapter
+//                productsRv.setAdapter(adapterProductSeller);
+//              }
+//
+//              @Override
+//              public void onCancelled(@Nonnull DatabaseError databaseError) {
+//
+//              }
+//            });
+//  }
+//
+//
+//  private void loadAllProducts() {
+//    productList = new ArrayList<>();
+//
+//    //get all product
+//    DatabaseReference reference = FirebaseDatabase.getInstance().getReference("User");
+//    reference.child(firebaseAuth.getUid()).child("Products")
+//            .addChildEventListener(new ValueEventListener() {
+//              @Override
+//              public void onDataChange(@Nonnull DataSnapshot dataSnapshot) {
+//                //before getting rest list
+//                productList.clear();
+//                for (DataSnapshot ds: dataSnapshot.getChildren()) {
+//                  ModelProduct modelProduct = ds.getValue(ModelProduct.class);
+//                  productList.add(modelProduct);
+//                }
+//                //setup adapter
+//                productsRv.setAdapter(adapterProductSeller);
+//                //set adapter
+//                productsRv.setAdapter(adapterProductSeller);
+//              }
+//
+//              @Override
+//              public void onCancelled(@Nonnull DatabaseError databaseError) {
+//
+//              }
+//            });
+//  }
+//
+//}
