c307d66a40b6ad6c209b09a2b806e5b45e4afb5e
OfirShitrit <ofirha18@gmail.com>
2024-02-04 12:36:14 +0200

    working

--- app/src/main/java/com/example/together/Client.java
+++ app/src/main/java/com/example/together/Client.java
@@ -1,5 +1,7 @@
 package com.example.together;
 
+import static android.content.ContentValues.TAG;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -7,11 +9,13 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -29,18 +33,23 @@ import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
+import java.util.Objects;
 
 import javax.annotation.Nonnull;
 
 public class Client extends AppCompatActivity {
     ImageButton btn_profile, btn_client_cart, btn_logout, filterProductBtn;
     Toolbar toolbar;
+    FirebaseFirestore db;
+
+    FirebaseAuth mAuth;
     private EditText searchProductsEt;
-    private TextView filteredProductsTv;
+    private TextView filteredProductsTv, cartCountTv;
     private RecyclerView productsRv;
     private FirebaseAuth firebaseAuth;
     private ArrayList<ModelProduct> productList;
     private AdapterProductClient adapterProductClient;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,10 +62,16 @@ public class Client extends AppCompatActivity {
         filteredProductsTv = findViewById(R.id.filteredProductsTv);
         filterProductBtn = findViewById(R.id.filterProductBtn);
         productsRv = findViewById(R.id.productsRv);
+        cartCountTv = findViewById(R.id.cartCountTv);
+        mAuth = FirebaseAuth.getInstance();
+
+
+        db = FirebaseFirestore.getInstance();
 
         firebaseAuth = FirebaseAuth.getInstance();
 
         loadProducts();
+//        cartCount();
 
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -96,7 +111,6 @@ public class Client extends AppCompatActivity {
     private void loadProducts() {
 
         productList = new ArrayList<>();
-        FirebaseFirestore db = FirebaseFirestore.getInstance();
 
 // Create a reference to the "sellers" collection
         CollectionReference sellersRef = db.collection("seller");
@@ -132,6 +146,8 @@ public class Client extends AppCompatActivity {
             }
         });
 
+
+
 //        //get all product
 //        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("seller");
 //        reference.child("products")
@@ -157,4 +173,42 @@ public class Client extends AppCompatActivity {
 //                });
 
     }
+
+    // TODO - not working return 6:16 (13)
+//    public void cartCount() {
+//        FirebaseUser user = mAuth.getCurrentUser();
+//        String userID = user.getUid();
+//        CollectionReference docRef = db.collection("clients").document(userID).collection("cart");
+//
+//        docRef
+//                .get()
+//                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+//                    @Override
+//                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+//                        if (task.isSuccessful()) {
+//                            int count = 0;
+//                            for (QueryDocumentSnapshot productDocument : task.getResult()) {
+//                                // Check if the "productTitle" field exists in the product document
+//                                if (productDocument.contains("productQuantity")) {
+//                                    long productQuantity = productDocument.getLong("productQuantity").intValue();; // 0 is the default value if the field is not present
+//                                    count += productQuantity;
+//
+//                                }
+//                            }
+//                            cartCountTv.setVisibility(View.VISIBLE);
+//                            cartCountTv.setText(String.valueOf(count));
+//                        } else {
+//                            Log.d(TAG, "Error getting documents: ", task.getException());
+////                            cartCountTv.setVisibility(View.GONE);
+//
+//                        }
+//                    }
+//                });
+//    }
+
+
+
+
+
+
 }
